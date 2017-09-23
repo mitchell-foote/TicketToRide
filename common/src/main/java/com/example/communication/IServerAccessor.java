f1b04689f5fb00496872b93d365751438953ac8d
Mitchell Foote <mitchellfoote93@gmail.com>
2017-09-22 21:19:07 -0600

    Changed the Color object used in the IServerAccessor class.
    Turns out you can't mix colors from the java library and the Android
    lib. So I made an enum to compensate. Only with colors we allow.

--- common/src/main/java/com/example/communication/IServerAccessor.java
+++ common/src/main/java/com/example/communication/IServerAccessor.java
@@ -1,9 +1,10 @@
 package com.example.communication;
 
 import com.example.model.classes.login.BaseGameSummary;
+import com.example.model.enums.SharedColor;
 
-import java.awt.Color;
 import java.util.List;
+
 
 /**
  * Created by Mitchell Foote on 9/22/2017.
@@ -13,7 +14,7 @@ public interface IServerAccessor
     String login(String userName, String password);
     String register(String username, String password);
     boolean logoff(String authToken);
-    boolean joinGame(String id, Color color, String authToken);
+    boolean joinGame(String id, SharedColor color, String authToken);
     boolean leaveGame(String id, String authToken);
     List<BaseGameSummary> getGames(String authToken);
 }
---
+++ common/src/main/java/com/example/model/enums/SharedColor.java
@@ -0,0 +1,18 @@
+package com.example.model.enums;
+
+/**
+ * Created by Mitchell Foote on 9/22/2017.
+ */
+public enum SharedColor
+{
+    RAINBOW,
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    PURPLE,
+    BLACK,
+    WHITE
+
+}
