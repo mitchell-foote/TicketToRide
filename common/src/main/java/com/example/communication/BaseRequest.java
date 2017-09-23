2d2280637cc86fae8772fee21d66aef42fed9756
Mitchell Foote <mitchellfoote93@gmail.com>
2017-09-22 21:22:39 -0600

    Added common request and response classes

---
+++ common/src/main/java/com/example/communication/BaseRequest.java
@@ -0,0 +1,10 @@
+package com.example.communication;
+
+/**
+ * Created by Mitchell Foote on 9/22/2017.
+ */
+public class BaseRequest
+{
+    String type;
+    Object body;
+}
---
+++ common/src/main/java/com/example/communication/BaseResponse.java
@@ -0,0 +1,13 @@
+package com.example.communication;
+
+/**
+ * Created by Mitchell Foote on 9/22/2017.
+ */
+public class BaseResponse
+{
+    Object response;
+    String type;
+    boolean hasError;
+    String errorText;
+
+}
