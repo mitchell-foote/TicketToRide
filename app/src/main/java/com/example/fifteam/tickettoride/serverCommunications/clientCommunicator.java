aad7b1b8ee9d74ba4d912145f234657e2632db23
Mitchell Foote <mitchellfoote93@gmail.com>
2017-09-22 21:40:55 -0600

    Added method names to clientCommunicator class

--- app/src/main/java/com/example/fifteam/tickettoride/serverCommunications/clientCommunicator.java
+++ app/src/main/java/com/example/fifteam/tickettoride/serverCommunications/clientCommunicator.java
@@ -1,8 +1,16 @@
 package com.example.fifteam.tickettoride.serverCommunications;
 
+import com.example.communication.BaseRequest;
+
 /**
  * Created by Mitchell Foote on 9/22/2017.
  */
 public class clientCommunicator
 {
+    public String get(String host, String port, String url, String authToken, BaseRequest body){
+        return null;
+    }
+    public String post(String host, String port, String url, String authToken, BaseRequest body){
+        return null;
+    }
 }
