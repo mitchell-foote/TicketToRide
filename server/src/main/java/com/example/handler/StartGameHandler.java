0db0ccca634fd57dd6469b053a81351344c653b7
BrittonBunn <britton.bunn@gmail.com>
2017-10-06 16:22:05 -0600

    Added StartGame Handler

---
+++ server/src/main/java/com/example/handler/StartGameHandler.java
@@ -0,0 +1,67 @@
+package com.example.handler;
+
+import com.example.Exceptions.FailedLoginException;
+import com.example.communication.BaseRequest;
+import com.example.communication.BaseResponse;
+import com.example.communication.IServerAccessor;
+import com.example.model.ServerFacade;
+import com.example.model.classes.users.LoginRequest;
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+
+/**
+ * Created by ninjup on 10/1/17.
+ */
+
+public class StartGameHandler extends Handler implements HttpHandler {
+
+    private IServerAccessor serverFacade = new ServerFacade();
+
+    @Override
+    public void handle(HttpExchange httpExch) throws IOException {
+
+        System.out.println("Start Game handler reached");
+
+        try {
+            if (httpExch.getRequestMethod().toLowerCase().equals("post")) {
+                InputStream requestBody = httpExch.getRequestBody();
+                OutputStream responseBody = httpExch.getResponseBody();
+
+                String authToken = httpExch.getRequestHeaders().getFirst("Authorization");
+
+                String requestData = this.readString(requestBody);
+                BaseRequest data = new Gson().fromJson(requestData, BaseRequest.class);
+                String gameId = (String) data.body;
+
+
+                try {
+
+                    if (serverFacade.startGame(gameId, authToken)) {
+                        BaseResponse response = new BaseResponse();
+                        response.type = "boolean";
+                        response.response = true;
+                        response.hasError = false;
+                        httpExch.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+                        writeString(new Gson().toJson(response), responseBody);
+                        responseBody.close();
+                    } else {
+                        userError(httpExch, new Exception("Start game failed"));
+                    }
+                } catch (Exception e) {
+                    userError(httpExch, e);
+                }
+            } else {
+                userError(httpExch, new Exception("Error: wrong request method used"));
+            }
+        } catch (IOException e) {
+            serverError(httpExch, e);
+            e.printStackTrace();
+        }
+    }
+}
