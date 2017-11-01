41e53bbe7c86cd56f471bf93e7af91a2e9cffb93
jared-haviland <jared_haviland@byu.edu>
2017-10-31 22:29:54 -0600

    Added all routes to the map (courtesy of Sam) - just need to draw the double routes as doubles

--- app/src/main/java/com/example/fifteam/tickettoride/views/GameMapView.java
+++ app/src/main/java/com/example/fifteam/tickettoride/views/GameMapView.java
@@ -209,77 +209,116 @@ public class GameMapView extends SupportMapFragment implements OnMapReadyCallbac
         routeList.add(new Route("12", SharedColor.RAINBOW, 3, City.Calgary, City.Vancouver));
         routeList.add(new Route("13", SharedColor.WHITE, 6, City.Calgary, City.Winnipeg));
 
+        routeList.add(new Route("14", SharedColor.RAINBOW, 4, City.Calgary, City.Helena));
+        routeList.add(new Route("15", SharedColor.RAINBOW, 4, City.Calgary, City.Seattle));
 
-        /*
-        routeList.add(new Route("14", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("15", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("16", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("17", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("18", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("19", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("20", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("21", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("22", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("23", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("24", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("25", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("26", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("27", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("28", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("29", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("30", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("31", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("32", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("33", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("34", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("35", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("36", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("37", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("38", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("39", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("40", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("41", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("42", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("43", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("44", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("45", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("46", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("47", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("48", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("49", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("50", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("51", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("52", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("53", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("54", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("55", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("56", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("57", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("58", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("59", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("60", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("61", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("62", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("63", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("64", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("65", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("66", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("67", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("68", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("69", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("70", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("71", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("72", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("73", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("74", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("75", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("76", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("77", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("78", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("79", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        routeList.add(new Route("80", SharedColor.RED, 3, City.San_Francisco, City.Los_Angeles));
-        */
+        routeList.add(new Route("16", SharedColor.RAINBOW, 2, City.Charleston, City.Raleigh));
+        routeList.add(new Route("17", SharedColor.RAINBOW, 2, City.Charleston, City.Atlanta));
+        routeList.add(new Route("18", SharedColor.PURPLE, 4, City.Charleston, City.Miami));
+        //double route
+        routeList.add(new Route("19", SharedColor.ORANGE, 3, City.Chicago, City.Pittsburgh));
+        routeList.add(new Route("20", SharedColor.BLACK, 3, City.Chicago, City.Pittsburgh));
+        routeList.add(new Route("21", SharedColor.WHITE, 4, City.Chicago, City.Toronto));
+        routeList.add(new Route("22", SharedColor.RED, 3, City.Chicago, City.Duluth));
+        routeList.add(new Route("23", SharedColor.BLUE, 4, City.Chicago, City.Omaha));
+        routeList.add(new Route("24", SharedColor.GREEN, 2, City.Chicago, City.Saint_Louis));
+        routeList.add(new Route("25", SharedColor.WHITE, 2, City.Chicago, City.Saint_Louis));
+        routeList.add(new Route("26", SharedColor.RAINBOW, 2, City.Dallas, City.Little_Rock));
+        //double route
+        routeList.add(new Route("27", SharedColor.RAINBOW, 2, City.Dallas, City.Oklahoma_City));
+        routeList.add(new Route("28", SharedColor.RAINBOW, 2, City.Dallas, City.Oklahoma_City));
+        routeList.add(new Route("29", SharedColor.RED, 4, City.Dallas, City.El_Paso));
+        //double route
+        routeList.add(new Route("30", SharedColor.RAINBOW, 1, City.Dallas, City.Houston));
+        routeList.add(new Route("31", SharedColor.RAINBOW, 1, City.Dallas, City.Houston));
+        //double route
+        routeList.add(new Route("32", SharedColor.ORANGE, 4, City.Denver, City.Kansas_City));
+        routeList.add(new Route("33", SharedColor.BLACK, 4, City.Denver, City.Kansas_City));
+        routeList.add(new Route("34", SharedColor.PURPLE, 4, City.Denver, City.Omaha));
+        routeList.add(new Route("35", SharedColor.GREEN, 4, City.Denver, City.Helena));
+        //double route
+        routeList.add(new Route("36", SharedColor.RED, 3, City.Denver, City.Salt_Lake_City));
+        routeList.add(new Route("37", SharedColor.YELLOW, 3, City.Denver, City.Salt_Lake_City));
+        routeList.add(new Route("38", SharedColor.WHITE, 5, City.Denver, City.Phoenix));
+        routeList.add(new Route("39", SharedColor.RAINBOW, 2, City.Denver, City.Santa_Fe));
+        routeList.add(new Route("40", SharedColor.RED, 4, City.Denver, City.Oklahoma_City));
+        //double route
+        routeList.add(new Route("41", SharedColor.RAINBOW, 2, City.Duluth, City.Omaha));
+        routeList.add(new Route("42", SharedColor.RAINBOW, 2, City.Duluth, City.Omaha));
+        routeList.add(new Route("43", SharedColor.RED, 3, City.Duluth, City.Chicago));
+        routeList.add(new Route("44", SharedColor.PURPLE, 6, City.Duluth, City.Toronto));
+        routeList.add(new Route("45", SharedColor.RAINBOW, 3, City.Duluth, City.Sault_Ste_Marie));
+        routeList.add(new Route("46", SharedColor.BLACK, 4, City.Duluth, City.Winnipeg));
+        routeList.add(new Route("47", SharedColor.ORANGE, 6, City.Duluth, City.Helena));
+        routeList.add(new Route("48", SharedColor.GREEN, 6, City.El_Paso, City.Houston));
+        routeList.add(new Route("49", SharedColor.YELLOW, 5, City.El_Paso, City.Oklahoma_City));
+        routeList.add(new Route("50", SharedColor.RAINBOW, 2, City.El_Paso, City.Santa_Fe));
+        routeList.add(new Route("51", SharedColor.RAINBOW, 3, City.El_Paso, City.Phoenix));
+        routeList.add(new Route("52", SharedColor.BLACK, 6, City.El_Paso, City.Los_Angeles));
+        routeList.add(new Route("53", SharedColor.BLUE, 4, City.Helena, City.Winnipeg));
+        routeList.add(new Route("54", SharedColor.RED, 5, City.Helena, City.Omaha));
+        routeList.add(new Route("55", SharedColor.PURPLE, 3, City.Helena, City.Salt_Lake_City));
+        routeList.add(new Route("56", SharedColor.YELLOW, 6, City.Helena, City.Seattle));
+        routeList.add(new Route("57", SharedColor.RAINBOW, 2, City.Houston, City.New_Orleans));
+        //double route
+        routeList.add(new Route("58", SharedColor.BLUE, 2, City.Kansas_City, City.Saint_Louis));
+        routeList.add(new Route("59", SharedColor.PURPLE, 2, City.Kansas_City, City.Saint_Louis));
+        //double route
+        routeList.add(new Route("60", SharedColor.RAINBOW, 1, City.Kansas_City, City.Omaha));
+        routeList.add(new Route("61", SharedColor.RAINBOW, 1, City.Kansas_City, City.Omaha));
+        //double route
+        routeList.add(new Route("62", SharedColor.RAINBOW, 2, City.Kansas_City, City.Oklahoma_City));
+        routeList.add(new Route("63", SharedColor.RAINBOW, 2, City.Kansas_City, City.Oklahoma_City));
+        routeList.add(new Route("64", SharedColor.ORANGE, 3, City.Las_Vegas, City.Salt_Lake_City));
+        routeList.add(new Route("65", SharedColor.RAINBOW, 2, City.Las_Vegas, City.Los_Angeles));
+        routeList.add(new Route("66", SharedColor.WHITE, 3, City.Little_Rock, City.Nashville));
+        routeList.add(new Route("67", SharedColor.RAINBOW, 2, City.Little_Rock, City.Saint_Louis));
+        routeList.add(new Route("68", SharedColor.RAINBOW, 2, City.Little_Rock, City.Oklahoma_City));
+        routeList.add(new Route("69", SharedColor.GREEN, 3, City.Little_Rock, City.New_Orleans));
+        //double route
+        routeList.add(new Route("70", SharedColor.PURPLE, 3, City.San_Francisco, City.Los_Angeles));
+        routeList.add(new Route("71", SharedColor.YELLOW, 3, City.San_Francisco, City.Los_Angeles));
+        routeList.add(new Route("72", SharedColor.RAINBOW, 3, City.Phoenix, City.Los_Angeles));
+        routeList.add(new Route("73", SharedColor.RED, 6, City.Miami, City.New_Orleans));
+        routeList.add(new Route("74", SharedColor.RAINBOW, 3, City.Montreal, City.New_York));
+        routeList.add(new Route("75", SharedColor.RAINBOW, 3, City.Montreal, City.Toronto));
+        routeList.add(new Route("76", SharedColor.BLACK, 5, City.Montreal, City.Sault_Ste_Marie));
+        routeList.add(new Route("77", SharedColor.BLACK, 3, City.Nashville, City.Raleigh));
+        routeList.add(new Route("78", SharedColor.YELLOW, 4, City.Nashville, City.Pittsburgh));
+        routeList.add(new Route("79", SharedColor.RAINBOW, 2, City.Nashville, City.Saint_Louis));
+        //double route
+        routeList.add(new Route("80", SharedColor.BLACK, 2, City.New_York, City.Washington));
+        routeList.add(new Route("81", SharedColor.ORANGE, 2, City.New_York, City.Washington));
+        //double route
+        routeList.add(new Route("82", SharedColor.WHITE, 2, City.New_York, City.Pittsburgh));
+        routeList.add(new Route("83", SharedColor.GREEN, 2, City.New_York, City.Washington));
+        routeList.add(new Route("84", SharedColor.BLUE, 3, City.Oklahoma_City, City.Santa_Fe));
+        routeList.add(new Route("85", SharedColor.RAINBOW, 3, City.Phoenix, City.Santa_Fe));
+        routeList.add(new Route("86", SharedColor.RAINBOW, 2, City.Pittsburgh, City.Washington));
+        routeList.add(new Route("87", SharedColor.RAINBOW, 2, City.Pittsburgh, City.Raleigh));
+        routeList.add(new Route("88", SharedColor.GREEN, 5, City.Pittsburgh, City.Saint_Louis));
+        routeList.add(new Route("89", SharedColor.RAINBOW, 2, City.Pittsburgh, City.Toronto));
+        routeList.add(new Route("90", SharedColor.RAINBOW, 1, City.Portland, City.Seattle));
+        routeList.add(new Route("91", SharedColor.RAINBOW, 1, City.Portland, City.Seattle));
+        routeList.add(new Route("92", SharedColor.BLUE, 6, City.Portland, City.Salt_Lake_City));
+        routeList.add(new Route("93", SharedColor.GREEN, 5, City.Portland, City.San_Francisco));
+        routeList.add(new Route("94", SharedColor.PURPLE, 5, City.Portland, City.San_Francisco));
+        routeList.add(new Route("95", SharedColor.RAINBOW, 2, City.Raleigh, City.Washington));
+        routeList.add(new Route("96", SharedColor.RAINBOW, 2, City.Raleigh, City.Washington));
+        routeList.add(new Route("97", SharedColor.ORANGE, 5, City.Salt_Lake_City, City.San_Francisco));
+        routeList.add(new Route("98", SharedColor.WHITE, 5, City.Salt_Lake_City, City.San_Francisco));
+        routeList.add(new Route("99", SharedColor.RAINBOW, 6, City.Sault_Ste_Marie, City.Winnipeg));
+        routeList.add(new Route("100", SharedColor.RAINBOW, 2, City.Sault_Ste_Marie, City.Toronto));
+        routeList.add(new Route("101", SharedColor.RAINBOW, 1, City.Seattle, City.Vancouver));
+        routeList.add(new Route("102", SharedColor.RAINBOW, 1, City.Seattle, City.Vancouver));
 
         return routeList;
+    }
+
+    //this method takes a SharedColor object and returns the corresponding color integer
+    private int getColorInt(SharedColor color) {
+
+
+        return 0;
     }
 }
